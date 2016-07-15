5c9eb08eceb6057e88b6f300bf5f79b2bf9318ba
Sebastian Borrazas <seba.borrazas@gmail.com>
2016-07-15 18:27:58 -0300

    Add basic font usage

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -40,6 +40,24 @@ module.exports = {
         test: webpackIsomorphicToolsPlugin.regular_expression("images"),
         loader: "url-loader?limit=153600",
       },
+      // FONTS
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      },
     ],
   },
 };
--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -49,6 +49,24 @@ module.exports = {
         test: webpackIsomorphicToolsPlugin.regular_expression("images"),
         loader: "url-loader?limit=153600",
       },
+      // FONTS
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff&name=/fonts/[hash].[ext]"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff&name=/fonts/[hash].[ext]"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream&name=/fonts/[hash].[ext]"
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml&name=/fonts/[hash].[ext]"
+      },
     ]
   },
   plugins: [
