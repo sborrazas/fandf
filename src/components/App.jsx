115bcf440e0a8d0acad9de4eb8f7ec1a8a6158db
Sebastian Borrazas <seba.borrazas@gmail.com>
2016-07-22 17:09:17 -0300

    Change react-router history type to browser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Router, hashHistory } from "react-router";
+import { Router, browserHistory } from "react-router";
 
 class App extends Component {
   render() {
     return (
-      <Router history={hashHistory} routes={this.props.routes} />
+      <Router history={browserHistory} routes={this.props.routes} />
     );
   }
 };
