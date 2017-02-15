44a49c8281dcbf23b0d3e2684ca7cfb0431f62f6
Sebastian Borrazas <seba.borrazas@gmail.com>
2017-02-15 14:13:12 -0300

    Update keywords

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export const NAME = "Form & Function";
 export const TAGLINE = "We design. We build. Together.";
 export const DESCRIPTION = "We are a small team of creative professionals who are passioned about partnering with purpose-driven businesses and startups to craft unique product solutions.";
-export const KEYWORDS = "montevideo, uruguay, apps, software, development, design, ux, studio, agency";
+export const KEYWORDS = "websites, studio, agency, mobile, web, apps, digital products, branding, software, development, design, ux";
 export const AUTHORS = "Sebastian Borrazas, Santiago Alonso";
 export const SOCIAL = {
   EMAIL: "hello@fandf.io",
